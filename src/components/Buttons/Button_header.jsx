c33afe232cd8445be873d4128a5879666876c5f2
slozhkai <sisipopakaka@gmail.com>
2024-03-17 22:12:36 +0300

    сделать модальное окно

--- src/components/Buttons/Button_header.jsx
+++ src/components/Buttons/Button_header.jsx
@@ -1,13 +1,50 @@
 import Lock from "../../img/lock.png";
 import React from "react";
+import './Button_header.css'
 
-function Button_header () {
-        return(
-            <div className={'button_reg'}>
-                <a>АВТОРИЗАЦИЯ</a>
-                <img src={Lock} alt={'lock'} className={'header__lock'} />
-            </div>
-        )
+function Button_header ({onClick}) {
+    const handleButtonClick = () => {
+      console.log('Button pressed')
+    }
+
+    return(
+        <div className={'button_reg'} onClick={onClick}>
+            <a className={'text'}>АВТОРИЗАЦИЯ</a>
+            <img src={Lock} alt={'lock'} className={'header__lock'} />
+        </div>
+    )
 }
 
 export default Button_header;
+
+import React, { useState } from "react";
+import Lock from "../../img/lock.png";
+import './Button_header.css'
+
+// function Button_header ({onClick}) {
+//     const [modalOpen, setModalOpen] = useState(false);
+//
+//     const handleButtonClick = () => {
+//         setModalOpen(true);
+//         onClick(); // Вызов функции обратного вызова, если необходимо
+//     };
+//
+//     const closeModal = () => {
+//         setModalOpen(false);
+//     };
+//
+//     return (
+//         <div>
+//             <div className={'button_reg'} onClick={handleButtonClick}>
+//                 <a className={'text'}>АВТОРИЗАЦИЯ</a>
+//                 <img src={Lock} alt={'lock'} className={'header__lock'} />
+//             </div>
+//             {modalOpen && (
+//                 <div className="modal">
+//                     <p>Модальное окно</p>
+//                     <button onClick={closeModal}>Закрыть</button>
+//                 </div>
+//             )}
+//         </div>
+//     );
+// }
